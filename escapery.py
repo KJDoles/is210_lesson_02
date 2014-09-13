7ebb14771b62f7efe998d607ec6e55a9a38527b2
KJDoles <kevin.doles@spsmail.cuny.edu>
2014-09-13 12:26:13 -0400

    Corrections numtypes, integer_math, escapery, simple_types, inquisition.

--- escapery.py
+++ escapery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-ESCAPE_STRING = r'\n'
+ESCAPE_STRING = '\\n'
 print ESCAPE_STRING
--- numtypes.py
+++ numtypes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 '''Expresses differences between integer, fraction, and decimal number types.
 '''
 
